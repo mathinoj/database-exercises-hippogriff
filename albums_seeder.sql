d2346b909cf0c6027d174cb1dc32479946fb6657
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-11-27 15:20:26 -0600

    added album information to albums seeders

---
+++ albums_seeder.sql
@@ -0,0 +1,24 @@
+use hippo_matthias;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 70, 'Pop, post-disco, funk, rock'),
+('AC/DC', 'Back in Black', 1980, 50, 'Hard rock'),
+('Whitney Houston', 'The Bodyguard', 1992, 45, 'R&B, soul, pop, soundtrack'),
+('Pink Floyd', 'Dark Side of the Moon', 1973, 45, 'Progressive rock'),
+('Eagles', 'Their Greatest Hits(1971-1975)', 1976, 44, 'Country, rock, soft rock, folk rock'),
+('Meat Loaf', 'Bat Out of Hell', 1977, 44, 'Hard rock, glam rock, progressive rock'),
+('Eagles', 'Hotel California', 1976, 42, 'Soft rock'),
+('Shania Twain', 'Come On Over', 1997, 40, 'Country, pop'),
+('Fleetwood Mac', 'Rumors', 1977, 40, 'Soft rock'),
+('Bee Gees', 'Saturday Night Fever', 1977, 40, 'Disco'),
+('Led Zepplin', 'Led Zepplin IV', 1971, 37, 'Hard rock, heavy metal, folk rock'),
+('Michael Jackson', 'Bad', 1987, 35, 'Pop, rhythm and blues, funk, and rock'),
+('Alanis Morissette', 'Jagged Little Pill', 1995, 33, 'Alternative rock'),
+('Various artists', 'Dirty Dancing', 1987, 32, 'Pop, rock, R&B'),
+('Celine Dion', 'Falling into You', 1996, 32, 'Pop, soft rock'),
+('Adele', '21', 2011, 31, 'Pop, soul'),
+('The Beatles', '1', 2000, 31, 'Rock'),
+('Metallica', 'Metallica', 1991, 31, 'Heavy metal'),
+('Celine Dion', 'Lets Talk About Love', 1997, 31, 'Pop, soft rock'),
+('Bob Marley and the Wailers', 'Legend: The Best of Bob Marley & the Wailers', 1984, 30, 'Reggae')
+
