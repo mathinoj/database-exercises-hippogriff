176c0f5a9cc6d5f5b154ea07f3d1b5ca8520840d
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-11-29 11:44:20 -0600

    Completed limit exercises

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -7,3 +7,7 @@ order by last_name desc limit 10;
 
 select emp_no from salaries
 order by salary desc limit 5;
+
+select emp_no from salaries
+order by salary desc limit 5 offset 45;
+# limit 5 * 10th page = 50// 50-5 = 45, we do minus 5 cuz were limiting 5 views per pg
