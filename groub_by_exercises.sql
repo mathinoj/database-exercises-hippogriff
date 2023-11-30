1bf3956894d5363cc90766dc3a9ae54b2a173fc8
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-11-30 10:44:13 -0600

    Completed exercise 4 in group by

--- groub_by_exercises.sql
+++ groub_by_exercises.sql
@@ -6,3 +6,7 @@ select distinct title from titles;
 # Find your query for employees whose last names start and end with 'E'. Update the query to find just the unique last names that start and end with 'E' using GROUP BY. The results should be:
 select unique last_name from employees
 where last_name like 'e%' and last_name like '%e' group by last_name;
+
+# Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
+select unique first_name, last_name from employees
+where last_name like 'e%' and last_name like '%e';
